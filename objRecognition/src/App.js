0d06f1766d653fd8bd759662a31cd5f37f615e46
Eric M Shen <eric.mm.shen@gmai.com>
2023-02-18 15:41:15 -0800

    add in cumulative detection chart

--- objRecognition/src/App.js
+++ objRecognition/src/App.js
@@ -2,8 +2,31 @@ import { useRef, useEffect, useState } from "react";
 import io from "socket.io-client";
 import Webcam from "react-webcam";
 import RobotArm from "./RobotArm.js";
-import "./App.css";
-
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Filler,
+  Tooltip,
+  Legend,
+ } from 'chart.js';
+ import { Line } from 'react-chartjs-2';
+ import "./App.css";
+ 
+ ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Filler,
+  Title,
+  Tooltip,
+  Legend
+ );
+ 
 const STREAM_WIDTH_RATIO = 0.7;
 const STREAM_HEIGHT_RATIO = 0.8;
 
@@ -93,23 +116,107 @@ function VideoStream({ detections }) {
   );
 }
 
+function DisplayChart({ newData }) {
+  const chartOptions = {
+    responsive: true,
+    animation: {
+      duration: 200,
+    },
+    elements: {
+      point: {
+        radius: 0,
+      }
+    },
+    plugins: {
+      title: {
+        display: false,
+      },
+      legend: {
+        display: false
+      },
+    },
+    scales: {
+      x: {
+        title: {
+          display: true,
+          text: "Time",
+        },
+        ticks: {
+          display: false
+        },
+        grid: {
+          display: false
+        },
+        min: 0,
+      },
+      y: {
+        title: {
+          display: true,
+          text: "Total Number of Detections",
+        },
+        grid: {
+          display: false
+        },
+        min: 0,
+      }
+    },
+  };
+ 
+  const [chartData, setChartData] = useState([]);
+ 
+  useEffect(() => {
+    const fetchData = () => {
+      // for new anomalies per second
+      // for total anomalies: just concat newData
+      // setChartData(chartData.concat([newData - last]));
+      setChartData(chartData.concat([newData]));
+    };
+ 
+    const intervalId = setInterval(fetchData, 1000); // update every second
+ 
+    return () => clearInterval(intervalId);
+  }, [chartData, newData]);
+ 
+  return (
+    <div >
+      <Line
+        options={chartOptions}
+        data={{
+          labels: Array.from({length: chartData.length}, (v, i) => i),
+          datasets: [
+            {
+              data: chartData,
+              tension: 0.2,
+              fill: true,
+              borderColor: 'rgb(52, 160, 164)',
+              backgroundColor: 'rgb(155, 195, 198)',
+            }
+          ],
+        }}
+      />
+    </div>
+  )
+}
+
 function App() {
   const windowSize = useRef([window.innerWidth, window.innerHeight]);
-  const [pillCount, setPillCount] = useState(0);
+  const [totalCount, setTotalCount] = useState(0);
+  const [newCount, setNewCount] = useState(0);
   const [detections, setDetections] = useState([]);
   document.body.style.overflow = "hidden";
   useEffect(() => {
     const socket = io("http://localhost:65534");
 
     socket.on("detections", (detections) => {
-      setPillCount(pillCount + 1);
+      setTotalCount(totalCount + detections.length);
+      setNewCount(detections.length);
       setDetections(detections);
     });
 
     return () => {
       socket.disconnect();
     };
-  }, [pillCount]);
+  }, [totalCount]);
 
   return (
     <div
@@ -163,6 +270,9 @@ function App() {
             }}
           >
             <RobotArm></RobotArm>
+          </div>
+          <div>
+          <DisplayChart newData={totalCount}/>
           </div>
         </div>
       </div>
