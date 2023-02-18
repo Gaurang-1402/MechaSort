7b62c7250af4a7d73feeb4b800f49b48f5b5fe22
Gaurang Ruparelia <gr2159@nyu.edu>
2023-02-18 01:15:32 -0800

    Added robo setup

---
+++ robot-code/setup.py
@@ -0,0 +1,17 @@
+import xarm
+
+arm = xarm.Controller('USB')
+
+servo1 = xarm.Servo(1)
+servo2 = xarm.Servo(2)
+
+
+# Dancing Logic
+arm.setPosition(2, 0, 2000, wait=False)
+arm.setPosition(1, 500, 2000, wait=True)
+arm.setPosition(2, 1000, 4000, wait=False)
+
+arm.setPosition(1, 0, 1000, wait=True)
+arm.setPosition(1, 500, 1000, wait=True)
+arm.setPosition(1, 0, 1000, wait=True)
+arm.setPosition(1, 500, 1000, wait=True)
